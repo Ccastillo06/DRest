aa927757f90a1a1aa2b29ca38e3a3614b540a936
popelini <ccastillon92@gmail.com>
2017-12-12 14:16:16 +0100

    CAN uPLOAD IMAGES TO CLOUDINARY

--- server/api/routes/restaurant.route.js
+++ server/api/routes/restaurant.route.js
@@ -1,11 +1,22 @@
+const cloudinary = require('cloudinary');
+const cloudinaryStorage = require('multer-storage-cloudinary');
 const express = require('express');
 const router = express.Router();
 const RestaurantController = require('../controllers/restaurant.controller');
 const multer = require('multer');
-const upload = multer({ dest: './public/uploads' });
+// const upload = multer({ dest: './public/uploads' })
+var storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: 'DRest',
+  allowedFormats: ['jpg', 'png'],
+  filename: function (req, file, cb) {
+    cb(undefined, 'my-file-name');
+  }
+});
+var parser = multer({ storage: storage });
 
 router.get('/all', RestaurantController.getAll);
 router.get('/info/:id', RestaurantController.getInfo);
-router.post('/new', upload.single('file'), RestaurantController.newRestaurant);
+router.post('/new', parser.single('file'), RestaurantController.newRestaurant);
 
 module.exports = router;
