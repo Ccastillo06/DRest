339325433e1936d0e7a679d534a7360153d7c2fb
popelini <ccastillon92@gmail.com>
2017-12-15 00:22:43 +0100

    UPDATE INVENTORY DINAMICALLY HELL YEAH

--- server/api/controllers/ticket.controller.js
+++ server/api/controllers/ticket.controller.js
@@ -3,7 +3,7 @@ const debug = require('debug')('server: Ticket');
 const Table = require('../models/table.model');
 const Restaurant = require('../models/restaurant.model');
 const Ticket = require('../models/ticket.model');
-const Inventory = require('../models/inventory.model');
+const User = require('../models/user.model');
 const Product = require('../models/product.model');
 
 module.exports.generateTicket = (req, res, next) => {
@@ -66,4 +66,28 @@ module.exports.freeOrders = (req, res, next) => {
   Table.findByIdAndUpdate(req.params.table_id, { $set: { orders: [] }}, {new: true})
     .then(table => res.status(200).json(table))
     .catch(e => res.status(500).json({ message: 'Something went wrong'}))
+}
+
+module.exports.updateInventory = (req, res, next) => {
+  if(req.user.role!='Manager'){
+    res.status(401).json({ message : 'Unauthorized'});
+    return
+  }
+  Restaurant.findById(req.user.works_in)
+    .then(restaurant => {
+      User.findById(restaurant.owner)
+        .then(boss => {
+          Product.find({"owner": boss._id})
+            .then(productList => {
+              productList.forEach(x => {
+                req.body.total_order.forEach(y => {
+                  if(x.name == y.name) {
+                    x.quantity = (parseInt(x.quantity) - parseInt(y.amount)).toString();
+                  }
+                })
+                x.save();
+              })
+            })
+          })
+        })
 }
--- server/api/routes/ticket.route.js
+++ server/api/routes/ticket.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const TicketController = require('../controllers/ticket.controller');
 
+router.post('/update', TicketController.updateInventory);
 router.post('/create/:table_id/:rest_id', TicketController.generateTicket)
 router.post('/free/:table_id/:rest_id', TicketController.freeOrders);
 
