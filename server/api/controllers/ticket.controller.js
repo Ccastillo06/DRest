6b8283e6d111d370f1890d9607cc7775d54181cc
popelini <ccastillon92@gmail.com>
2017-12-15 17:46:31 +0100

    Corrected ticket price

--- server/api/controllers/ticket.controller.js
+++ server/api/controllers/ticket.controller.js
@@ -59,7 +59,7 @@ module.exports.generateTicket = (req, res, next) => {
             })
           })
           const theticket = new Ticket ({
-            total_price,
+            total_price = total_price.toFixed(2),
             table_name,
             total_order,
           });
